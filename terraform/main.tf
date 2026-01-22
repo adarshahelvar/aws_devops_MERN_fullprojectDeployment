c12d5d0e7ed1c6ca76ab4cf09e757c3a04954f8a
adarshahelvar <adarshaadi1997@gmail.com>
2026-01-22 07:47:11 +0530

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,42 @@
+resource "aws_security_group" "devops_sg" {
+  name = "devops-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "devops_ec2" {
+  ami           = "ami-019715e0d74f695be"   # ubuntu (Mumbai)
+  instance_type = "t2.micro"
+  key_name      = "aug07"      # YOUR_KEY_PAIR_NAME without extension
+  security_groups = [aws_security_group.devops_sg.name]
+
+  tags = {
+    Name = "devops-ec2"
+  }
+}
